bdb5e8de5dad72ed4777c49d86218649a5d78a0a
JayrajsinhB <jdbabariya04@gmail.com>
2025-07-16 18:18:50 +0530

    Add failing test for delete_sweet method

--- test_shop.py
+++ test_shop.py
@@ -1,15 +1,24 @@
 import unittest
 from shop import SweetShop
+
 class TestSweetShop(unittest.TestCase):
     def test_add_sweet(self):
         shop = SweetShop()
-        # shop.add_sweet("ID", "Name", "Category", "Price ", "Quantity")
-        shop.add_sweet("1001", "Kaju Katli", "Nut-Based", "50 ", "20")
-        self.assertEqual(shop.sweets["1001"]["Name"], "Kaju Katli")
+        
+        # add_sweet(id, name, category, price, quantity)
+        shop.add_sweet("1001", "Kaju Katli", "Nut-Based", 50, 20)
         
         result = shop.get_all_sweets()
         self.assertEqual(len(result), 1)
-        self.assertEqual(result[0],["name"], "Kaju Katli")
+        
+        
+        sweet = result[0]  # first sweet in list
+        self.assertEqual(sweet["id"], "1001")
+        self.assertEqual(sweet["name"], "Kaju Katli")
+        self.assertEqual(sweet["category"], "Nut-Based")
+        self.assertEqual(sweet["price"], 50)
+        self.assertEqual(sweet["quantity"], 20)
+
         
 if __name__ == '__main__':
     unittest.main()
