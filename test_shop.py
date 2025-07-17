8dad3626f871dbb35525c01a1f6e32b818479a8b
JayrajsinhB <jdbabariya04@gmail.com>
2025-07-17 17:30:37 +0530

    Add failing test for search_sweets by price range

--- test_shop.py
+++ test_shop.py
@@ -2,6 +2,8 @@ import unittest
 from shop import SweetShop
 
 class TestSweetShop(unittest.TestCase):
+    
+    # add sweet function
     def test_add_sweet(self):
         shop = SweetShop()
         
@@ -53,7 +55,17 @@ class TestSweetShop(unittest.TestCase):
         self.assertEqual(len(results), 2)
         self.assertEqual(results[0]["name"], "Gulab Jamun")
         self.assertEqual(results[1]["name"], "Rasgulla")
-
+        
+    def test_search_sweets_by_price_range(self):
+        shop = SweetShop()
+        shop.add_sweet("1001", "Kaju Katli", "Nut-Based", 50, 20)
+        shop.add_sweet("1002", "Gulab Jamun", "Milk-Based", 30, 10)
+        shop.add_sweet("1003", "Rasgulla", "Milk-Based", 70, 15)
+        
+        results = shop.search_sweets(price_range = (40, 60))
+        
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0]["name"], "Kaju Katli")
     
 if __name__ == '__main__':
     unittest.main()
